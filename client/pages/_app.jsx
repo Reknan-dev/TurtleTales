20f17730028feeacdd937d966a5edf74218eafbd
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-17 18:16:06 +0200

    favicon

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -6,7 +6,12 @@ import { store, persistor } from "../store/store";
 
 export default function MyApp({ Component, pageProps }) {
   return (
+
     <Provider store={store}>
+          <Head>
+    <link rel="icon" href="/favicon.ico" />
+    
+  </Head>
       <PersistGate loading={null} persistor={persistor}>
         <div className="min-h-screen bg-[url('/images/background.jpg')] bg-cover bg-center">
           <Component {...pageProps} />
