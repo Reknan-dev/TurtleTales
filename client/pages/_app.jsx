b05ac094ff228594f15bcf161f2a26467ed380f0
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-17 17:08:04 +0200

    added favicon

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -3,6 +3,7 @@ import { PersistGate } from "redux-persist/integration/react";
 import "../style/global.css";
 import { store, persistor } from "../store/store";
 
+
 export default function MyApp({ Component, pageProps }) {
   return (
     <Provider store={store}>
