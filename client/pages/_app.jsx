a950a71a5c783e627c6652144c2639b0da696839
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-20 15:29:17 +0200

    added Head

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -2,17 +2,17 @@ import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import "../style/global.css";
 import { store, persistor } from "../store/store";
-
+import Head from "next/head";
 
 export default function MyApp({ Component, pageProps }) {
   return (
 
     <Provider store={store}>
-       <head>
+       <Head>
         <title>Turtle Tales</title>
         <meta name="Turtle Tales" content="A social platform for help us to save turtles and fight the destruction of their nests" />
         <link rel="icon" href="/images/favicon.ico" />
-      </head>
+      </Head>
       <PersistGate loading={null} persistor={persistor}>
         <div className="min-h-screen bg-[url('/images/background.jpg')] bg-cover bg-center">
           <Component {...pageProps} />
