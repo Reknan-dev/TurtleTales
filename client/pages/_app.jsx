22c5d28f757608993a217c2fb2a54798ab56784a
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-20 15:18:53 +0200

    added meta tag

--- client/pages/_app.jsx
+++ client/pages/_app.jsx
@@ -8,6 +8,11 @@ export default function MyApp({ Component, pageProps }) {
   return (
 
     <Provider store={store}>
+       <Head>
+        <title>Turtle Tales</title>
+        <meta name="Turtle Tales" content="A social platform for help us to save turtles and fight the destruction of their nests" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
       <PersistGate loading={null} persistor={persistor}>
         <div className="min-h-screen bg-[url('/images/background.jpg')] bg-cover bg-center">
           <Component {...pageProps} />
