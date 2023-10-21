3eeef2af55df1a90fd1dbc004985f6f29cdcafc7
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-21 15:14:00 +0200

    map

--- client/components/MapComponent.jsx
+++ client/components/MapComponent.jsx
@@ -131,7 +131,7 @@ const ArcGISMapPage = () => {
   };
 
   return (
-    <div className="map-container">
+    <div className="map-container flex">
       <div className="bg-sand flex flex-col justify-center items-center pb-12 pt-12">
         <p className="mb-2.5 font-bold text-3xl text-center w-4/5">
           Click on the map to report sightings of hatches or sea turtles
