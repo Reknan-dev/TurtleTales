e5f7101a0f1e5fd1f2b4788e2d5cfdf58c874ce3
Reknan-dev <benedettocaiazzo8409@hotmail.com>
2023-10-22 15:33:12 +0200

    main

--- client/components/Main.jsx
+++ client/components/Main.jsx
@@ -4,7 +4,7 @@ export default function Main() {
   return (
     <main className="bg-[url('/images/banner.jpg')] flex w-full h-96 bg-cover bg-center justify-center items-center flex-col">
       <p className="title text-turtle text-5xl">
-        <strong>Help us to find them</strong>
+        <strong>Help to find them</strong>
       </p>
       <Link href="#mapDiv">
         <button className="join-button font-bold mt-3 w-28 px-4 py-2 hover:bg-scarlet hover:border-scarlet border-2 border-white rounded-3xl text-white duration-500">
